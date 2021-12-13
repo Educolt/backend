ec26fbcba3d37757f4ad908b5df8d2bf69f3cf6e
Educolt <Edupiece@hotmail.com>
2021-12-13 16:52:52 -0300

    add .gitignore file

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,9 +4,9 @@ export const userRoutes = Router();
 
 // rotas
 userRoutes.get('/', (request, response) => {
-    return response.status(200).json({message: 'Users routes - Its Alive !'})
+    return response.status(200).json({message: 'Users get routes - Its Alive !'})
 });
 userRoutes.post('/', (request, response) => {
     const {} = request.body;
-    return response.status(201).json({message: 'Users routes - Its Alive !'})
+    return response.status(201).json({message: 'Users post routes - Its Alive !'})
 });
