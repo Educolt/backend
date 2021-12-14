68f319cd512616e938600047f997f52a4bd6d4af
Educolt <Edupiece@hotmail.com>
2021-12-14 13:26:32 -0300

    .

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_1 = require("./App");
-App_1.app.listen(3333, () => {
-    console.log('Application start running 🚀!');
-});
